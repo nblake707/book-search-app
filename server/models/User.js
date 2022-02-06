52ce6036ad31ad0cfd77b7222f384d827b20c735
nblake707 <33844526+nblake707@users.noreply.github.com>
2022-02-05 21:35:41 -0500

    removed bcrypt

--- server/models/User.js
+++ server/models/User.js
@@ -1,5 +1,5 @@
 const { Schema, model } = require('mongoose');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 
 // import schema from Book.js
 const bookSchema = require('./Book');
@@ -43,9 +43,9 @@ userSchema.pre('save', async function (next) {
 });
 
 // custom method to compare and validate password for logging in
-userSchema.methods.isCorrectPassword = async function (password) {
-  return bcrypt.compare(password, this.password);
-};
+// userSchema.methods.isCorrectPassword = async function (password) {
+//   return bcrypt.compare(password, this.password);
+// };
 
 // when we query a user, we'll also get another field called `bookCount` with the number of saved books we have
 userSchema.virtual('bookCount').get(function () {
